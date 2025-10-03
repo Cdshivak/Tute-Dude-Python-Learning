590e52e5b3b37b92e7f1ed519c0d9e0af1ed7a08
Cdshivak <cdshivak@gmail.com>
2025-10-03 23:53:22 +0530

    my first assignment

---
+++ Assignment1.py
@@ -0,0 +1,26 @@
+            #Task1
+# Perform Basic Operations
+
+a1=(float(input("Enter the first value:")))
+b1=(float(input("enter your Second value:")))
+
+c1= a1 + b1
+print('Addition:', c1)
+
+c1 = a1 * b1
+print('Multiplication:', c1)
+
+c1 = a1 - b1
+print ('Subtraction:', c1)
+
+c1 = a1/b1
+print('Multiplication:' ,c1)
+
+
+
+# Task 2
+
+First_name= input('Enter your First name:')
+Last_name= input('Enter your Second Name:')
+
+print (f'Hello,{First_name} {Last_name}! Welcome to Python class')
